7de36ba395a393c497be64581cc54bd320c9c2f0
MartinGC94 <MGC94@outlook.dk>
2026-05-02 22:53:18 +0200

    Add preliminary support for monitor specialization

---
+++ src/DisplayConfig/Native/Structs/DISPLAYCONFIG_GET_MONITOR_SPECIALIZATION.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MartinGC94.DisplayConfig.Native.Structs
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct DISPLAYCONFIG_GET_MONITOR_SPECIALIZATION
+    {
+        public DISPLAYCONFIG_DEVICE_INFO_HEADER header;
+        public uint value;
+        public bool IsSpecializationEnabled => (value << 31) >> 31 == 1;
+        public bool IsSpecializationAvailableForMonitor => (value << 30) >> 31 == 1;
+        public bool IsSpecializationAvailableForSystem => (value << 29) >> 31 == 1;
+    }
+}
